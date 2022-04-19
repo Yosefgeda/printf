5eef08fb72ad6f391aaf819cbf9135491963d70b
josefgeda <dxrjos@gmail.com>
2022-04-19 08:56:02 +0100

    printf

---
+++ printf.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - function that returns number of characters printed
+ * @format: character string
+ * Return: integer
+ */
+
+int _printf(const char *format, ...)
+{
+int i;
+
+va_list character[];
+va_start(character[], format);
+
+for (i = 0 ; character[i] < '\0' ; i++ )
+num = va_arg(character[i], int);
+return (num);
+
+}
